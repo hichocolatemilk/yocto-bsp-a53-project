3d63b62675d8f025d0b8fb78bd6bdbc58c55ff4a
hello <hello@DESKTOP-EKF3LI2.localdomain>
2026-04-02 23:25:14 +0900

    tcp_secure_client add

---
+++ program/build/CMakeFiles/tcp_secure_client.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hello/program/src/openssl/AES256-CBC_TEST.c" "CMakeFiles/tcp_secure_client.dir/src/openssl/AES256-CBC_TEST.c.o" "gcc" "CMakeFiles/tcp_secure_client.dir/src/openssl/AES256-CBC_TEST.c.o.d"
+  "/home/hello/program/src/openssl/sha256_TEST.c" "CMakeFiles/tcp_secure_client.dir/src/openssl/sha256_TEST.c.o" "gcc" "CMakeFiles/tcp_secure_client.dir/src/openssl/sha256_TEST.c.o.d"
+  "/home/hello/program/src/socket/tcp/tcpClientThread.c" "CMakeFiles/tcp_secure_client.dir/src/socket/tcp/tcpClientThread.c.o" "gcc" "CMakeFiles/tcp_secure_client.dir/src/socket/tcp/tcpClientThread.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ program/build/CMakeFiles/tcp_secure_client.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_secure_client.dir/src/openssl/AES256-CBC_TEST.c.o"
+  "CMakeFiles/tcp_secure_client.dir/src/openssl/AES256-CBC_TEST.c.o.d"
+  "CMakeFiles/tcp_secure_client.dir/src/openssl/sha256_TEST.c.o"
+  "CMakeFiles/tcp_secure_client.dir/src/openssl/sha256_TEST.c.o.d"
+  "CMakeFiles/tcp_secure_client.dir/src/socket/tcp/tcpClientThread.c.o"
+  "CMakeFiles/tcp_secure_client.dir/src/socket/tcp/tcpClientThread.c.o.d"
+  "tcp_secure_client"
+  "tcp_secure_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tcp_secure_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
