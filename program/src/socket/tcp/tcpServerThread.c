94566f5da31ad1524ad208d44a91858db39cb43a
hello <hello@DESKTOP-EKF3LI2.localdomain>
2026-05-23 18:09:53 +0900

    tcpserverThread fix

--- program/src/socket/tcp/tcpServerThread.c
+++ program/src/socket/tcp/tcpServerThread.c
@@ -10,6 +10,7 @@ unsigned char iv[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 
 void *client_send(void *args){
     int client_sockfd = *(int *)args;
+    free(args);
     unsigned int ciphertext_len = 0;
     unsigned int decryptedtext_len = 0;
     int buffer_len;
@@ -39,7 +40,7 @@ void *client_send(void *args){
 
 int main(){
     pthread_t client_thread_id;
-    int sockfd, client_sockfd;
+    int sockfd;
     char message[BUFFER_SIZE];
     struct sockaddr_in server_addr, client_addr;
     socklen_t server_addr_len = sizeof(server_addr);
@@ -52,8 +53,9 @@ int main(){
     bind(sockfd, (struct sockaddr *)&server_addr, server_addr_len);
     listen(sockfd, 3);
     while(1){
-        client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
-        pthread_create(&client_thread_id, NULL, client_send, &client_sockfd);
+        int *pclient = malloc(sizeof(int));
+        *pclient = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+        pthread_create(&client_thread_id, NULL, client_send, (void *)pclient);
         pthread_detach(client_thread_id);
     }
     close(sockfd);
