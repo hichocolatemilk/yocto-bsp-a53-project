0ddb8fa0c5ce9037d551348cd28be822efd1ec8b
hello <hello@DESKTOP-EKF3LI2.localdomain>
2026-07-25 16:58:28 +0900

    add platform driver

--- meta-custom-a53/recipes-kernel/linux/linux-raspberrypi_%.bbappend
+++ meta-custom-a53/recipes-kernel/linux/linux-raspberrypi_%.bbappend
@@ -1,8 +1,7 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
-SRC_URI +=  "file://devtool-fragment.cfg \
-            file://0001-bcm2710-rpi3b-uart0-enable-lec_act-heartbeat.patch \
-	    file://0002-dts-change-serial0-to-uart0-set-pins-to-alt5.patch \
-            "
+SRC_URI +=  "file://fragment.cfg \
+	     file://0001-custom-a53-all.patch \           
+	    "
 
 
--- meta-custom-a53/recipes-kernel/my-driver/files/chartest.c
+++ meta-custom-a53/recipes-kernel/my-driver/files/chartest.c
@@ -5,62 +5,51 @@
 #include <linux/string.h>  //strlen
 #include <linux/errno.h> // EFAULT
 #include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/mod_devicetable.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("char driver");
 MODULE_DESCRIPTION("A simple char driver kernel module");
 
-static int __init Char_init(void);
-static int Char_open(struct inode *inode, struct file *filp);
-static int Char_release(struct inode *inode, struct file *filp);
-static ssize_t Char_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t Char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
-static void __exit Char_exit(void);
+// static int __init Char_init(void);
+// static int char_open(struct inode *inode, struct file *filp);
+// static int char_release(struct inode *inode, struct file *filp);
+// static ssize_t char_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+// static ssize_t char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
+// static void __exit Char_exit(void);
 
 static int major;
 static char driver_buf[100];
 static int stored_len = 0;  
 static struct class *cls;
 
-struct file_operations fops ={
-    .owner = THIS_MODULE,
-    .read = Char_read,
-    .write = Char_write,
-    .open = Char_open,
-    .release = Char_release
- };
+//기존 방식 char 그냥 선언
+// static int __init char_init(void) {
+//     major = register_chrdev(0, "chartest", &fops);
+//     if(major < 0){
+//         printk(KERN_CRIT "major failed %d\n", major);
+//         return major;
+//     }
+//     printk(KERN_NOTICE "Char Init!\n");
 
-// Called when module is loaded
-static int __init Char_init(void) {
-    major = register_chrdev(0, "chartest", &fops);
-    if(major < 0){
-        printk(KERN_CRIT "major failed %d\n", major);
-        return major;
-    }
-    printk(KERN_NOTICE "Char Init!\n");
-    // cls = class_create(THIS_MODULE, "chartest"); //6.4이전
-    cls = class_create("chartest"); // 6.4 이후
-    device_create(cls, NULL, MKDEV(major,0), NULL, "chartest");
-    pr_info("Major number %d\n", major);
-    pr_info("Device file : /dev/%s\n", "chartest");
-
-	return 0; 
-}
+// 	return 0; // Return 0 means succe
+// }
 
 //open echo로하는거에는 크게 상관?없을거같음.
-static int Char_open(struct inode *inode, struct file *filp){
+static int char_open(struct inode *inode, struct file *filp){
     printk(KERN_INFO "Char_open\n");
     return 0;
 }
 
 //close echo로하는거에는 크게 상관?없을거같음.
-static int Char_release(struct inode *inode, struct file *filp){
+static int char_release(struct inode *inode, struct file *filp){
     printk(KERN_INFO "Char_close\n");
     return 0;
 }
 
 //
-static ssize_t Char_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
+static ssize_t char_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
    
     if (*off >= stored_len)
         return 0;
@@ -78,7 +67,7 @@ static ssize_t Char_read(struct file *filp, char __user *buf, size_t len, loff_t
 }
 
 
-static ssize_t Char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off){
+static ssize_t char_write(struct file *filp, const char __user *buf, size_t len, loff_t *off){
    
     if (len > 100) 
         len = 100;
@@ -93,13 +82,61 @@ static ssize_t Char_write(struct file *filp, const char __user *buf, size_t len,
     return len;
 }
 
-// Called when module is removed
-static void __exit Char_exit(void) {
+static struct file_operations fops ={
+    .owner = THIS_MODULE,
+    .read = char_read,
+    .write = char_write,
+    .open = char_open,
+    .release = char_release
+ };
+
+
+//compatilbe을 보고 있으면 probe하는 방식
+static int char_probe(struct platform_device *pdev){
+    
+    major = register_chrdev(0, "chartest", &fops);
+    if(major < 0){
+        printk(KERN_CRIT "major failed %d\n", major);
+        return major;
+    }
+    // cls = class_create(THIS_MODULE, "chartest"); //6.4이전
+    cls = class_create("chartest"); // 6.4 이후
+    device_create(cls, NULL, MKDEV(major,0), NULL, "chartest");
+    printk(KERN_NOTICE "Platform Device Matched!...\n");
+    printk(KERN_NOTICE "Char Init!\n");
+
+	return 0; // Return 0 means succe
+}
+
+static int char_remove(struct platform_device *pdev){
     device_destroy(cls, MKDEV(major,0));
     class_destroy(cls);
     unregister_chrdev(major, "chartest");
-	printk(KERN_INFO "Goodbye, chartest!\n");
+    printk(KERN_INFO "Platform Device Removed!...\n");
+	printk(KERN_INFO "Goodbye, driver!\n");
+    return 0;
 }
 
-module_init(Char_init);
-module_exit(Char_exit);
+//dts의 compatble과 맞춰야함
+static const struct of_device_id my_dev_match[] = {
+    { .compatible = "my_chartest"},
+    {                            }
+};
+
+MODULE_DEVICE_TABLE(of, my_dev_match);
+
+//name은 위의 match 테이블과 이름이 같아야함.
+static struct platform_driver char_platform_driver = {
+    .probe = char_probe,
+    .remove = char_remove,
+    .driver = {
+        .name = "my_chartest",
+        .owner = THIS_MODULE,
+        .of_match_table = my_dev_match, // 위에 만든 매칭 테이블 등록
+    },
+};
+
+// Called when module is loaded
+// module_init(char_init);
+// module_exit(char_exit);
+module_platform_driver(char_platform_driver);
